f4ef7134e5957ec0024a41d7689bb90aa19d3372
GrimTrickster <grim.trickster04@gmail.com>
2022-09-05 12:31:04 +0200

    Updates

---
+++ precache-manifest.78e6c74fca412de38e874f817b0fa20d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f06a4f2dda9f343718124d79c9ed4a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed51d398ae5748537208",
+    "url": "/static/css/main.f2cf86ba.chunk.css"
+  },
+  {
+    "revision": "d64b4e8d8f8fdd52de47",
+    "url": "/static/js/2.8ac4c22e.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.8ac4c22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed51d398ae5748537208",
+    "url": "/static/js/main.d587cec7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d587cec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
